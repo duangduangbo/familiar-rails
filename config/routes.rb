f81b3bdb2c071b7e26042772f868d78c90cdae03
wenlingang <gang@tanmer.com>
2017-12-04 23:43:32 +0800

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :home
   root :to => "home#index"
+  resources :home
+  resources :users
+  get "login" => "users#login", :as => "login"
+  delete "logout" => "users#logout", :as => "logout"
+  post "sign_in" => "users#sign_in"
+
 end
